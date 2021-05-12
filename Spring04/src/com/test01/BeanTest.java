0906abfcd55ac47a122bce9f7460eb58976a0872
NamMinHyuk <skaalsgur156@naver.com>
2021-05-12 21:09:15 +0900

    spring2회차

--- Spring04/src/com/test01/BeanTest.java
+++ Spring04/src/com/test01/BeanTest.java
@@ -1,6 +1,9 @@
 package com.test01;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class BeanTest {
 
@@ -24,10 +27,38 @@ public class BeanTest {
 	public void setNumber(int num) {
 		System.out.println("setNumber(int num) 호출 : " + num);
 	}
+	
 	public void setArray(String[] arr) {
 		System.out.println("setArray(String[] arr) 호출");
 		for(String s : arr) {
 			System.out.println(s);
+		}
+	}
+	
+	public void setList(List<String> list) {
+		System.out.println("setList(List<String> list) 호출");
+		for(String s: list) {
+			System.out.println(s);
+		}
+	}
+	
+	public void setSet(Set<String> set) {
+		System.out.println("setSet(Set<String> set) 호출");
+		for(String s : set) {
+			System.out.println(s);
+		}
+	}
+	public void setMap(Map<String, String> map) {
+		System.out.println("setMap(Map<String, String> map) 호출");
+		// map의 value들만 출력하자.
+		for(String s : map.values()) {
+			System.out.println(s);
+		}
+	}
+	public void setScore(List<Score> list) {
+		System.out.println("setScore(List<Score> list) 호출");
+		for(Score sc : list) {
+			System.out.println(sc);
 		}
 	}
 }
